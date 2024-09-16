348de86529f35f0f9f242efdb048d1abd31d247a
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2024-09-16 19:21:18 +0200

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1>MoKri's Multilingual Dictionary</h1>
+      <h1 className="text-3xl font-bold underline text-red-600">MoKri's Multilingual Dictionary</h1>
     </>
   )
 }
