61f304d5b2156b1c059dec58f1d62e39b6d61ed0
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2024-09-22 16:11:44 +0200

    implement GET words fetching on the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import GreetingPage from "./pages/GreetingPage";
 import MainPage from "./pages/MainPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -12,8 +13,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/features/Words/models.ts
@@ -0,0 +1,8 @@
+export interface Word {
+    article?: string,
+    text: string,
+    plural?: string,
+    type: number,
+    conjugation?: string,
+    languageCode: number
+}
