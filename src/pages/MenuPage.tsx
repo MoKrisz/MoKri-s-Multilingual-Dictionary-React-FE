e0d11a8f325a1d48895a0f39a3e9623ed5d68d07
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-01-14 18:23:57 +0100

    add icons to table headers

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { getLanguageName, getWordTypeName } from "../features/Words/utils";
 import { ReactNode } from "react";
 import { BsPencilFill } from "react-icons/bs";
+import ColumnOrderIcon from "../components/ColumnOrderIcon";
 
 export default function MenuPage() {
   const { data, isPending, isError } = useQuery({
@@ -26,17 +27,35 @@ export default function MenuPage() {
       <table className="w-2/3 mt-10 border border-black">
         <thead className="bg-lincolngreen">
           <tr>
-            <th className="border border-black">Article</th>
-            <th className="border border-black">Word</th>
-            <th className="border border-black">Type</th>
-            <th className="border border-black">Language</th>
+            <th className="border border-black">
+              <div className="flex items-center justify-between mx-4 my-2">
+                <span>Article</span> <ColumnOrderIcon />
+              </div>
+            </th>
+            <th className="border border-black">
+              <div className="flex items-center justify-between mx-4 my-2">
+                <span>Word</span> <ColumnOrderIcon />
+              </div>
+            </th>
+            <th className="border border-black">
+              <div className="flex items-center justify-between mx-4 my-2">
+                <span>Type</span> <ColumnOrderIcon />
+              </div>
+            </th>
+            <th className="border border-black">
+              <div className="flex items-center justify-between mx-4 my-2">
+                <span>Language</span> <ColumnOrderIcon />
+              </div>
+            </th>
             <th className="border border-black"></th>
           </tr>
         </thead>
         <tbody>
           {data.map((word) => (
             <tr key={word.wordId} className="bg-lincolngreenlighter">
-              <td className="border border-black text-center">{word.article}</td>
+              <td className="border border-black text-center">
+                {word.article}
+              </td>
               <td className="border border-black text-center">{word.text}</td>
               <td className="border border-black text-center">
                 {getWordTypeName(word.type)}
@@ -45,7 +64,10 @@ export default function MenuPage() {
                 {getLanguageName(word.languageCode)}
               </td>
               <td className="border border-black p-1">
-                <Link to={`word/${word.wordId}`} className="inline-block align-middle">
+                <Link
+                  to={`word/${word.wordId}`}
+                  className="inline-block align-middle"
+                >
                   <BsPencilFill className="p-1 h-7 w-8 border border-black rounded-lg bg-green-900 fill-white hover:bg-green-600" />
                 </Link>
               </td>
