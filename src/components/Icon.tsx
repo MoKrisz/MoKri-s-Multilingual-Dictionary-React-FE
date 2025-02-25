96f41a5ec6ab6c5bca97a202685ad79f0f8a2745
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-02-25 22:08:17 +0100

    fix icon style

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -7,11 +7,11 @@ interface IconProps {
 
 export default function Icon({onClick, isDisabled = false, isSelected = false, children}: IconProps) {
     const disabledStyle = isDisabled ? " opacity-75" : "";
-    const selectedStyle = isSelected ? " bg-green-600" : "";
+    const backgroundColor = isSelected ? " bg-green-600" : " bg-green-900";
     const hoverStyle = isDisabled || isSelected ? "" : " hover:bg-green-600";
 
     const iconProps = {
-        className: `p-1 h-7 w-8 place-items-center border border-black rounded-lg bg-green-900 text-white${disabledStyle + selectedStyle + hoverStyle}`,
+        className: `p-1 h-7 w-8 place-items-center border border-black rounded-lg  text-white${disabledStyle + backgroundColor + hoverStyle}`,
         disabled: isDisabled,
         onClick
       };
