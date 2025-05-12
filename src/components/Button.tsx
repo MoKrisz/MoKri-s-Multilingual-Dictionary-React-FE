41662286654bd03f28d37246e2b2f7278177cc8d
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-05-12 19:36:23 +0200

    make Button component a bit more flexible

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 interface ButtonProps {
   children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
 }
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({ children, onClick, className }: ButtonProps) {
   return (
-    <button className="bg-lincolngreen font-bold p-2 rounded-lg border border-black hover:bg-lincolngreenlighter">
+    <button className={`bg-lincolngreen p-2 rounded-lg border border-black hover:bg-lincolngreenlighter ${className}`} onClick={onClick}>
       {children}
     </button>
   );
