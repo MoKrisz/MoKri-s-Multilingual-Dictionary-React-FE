32f29750c817c6a7ff041952bcb25c2f207fb2d7
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-06-17 20:59:13 +0200

    fix pagination bug - change current page to a valid page if the new max amount of pages is lower than the current page

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -3,41 +3,68 @@ import { PaginationData } from "../components/Pagination";
 import { DataAmountPerPage } from "../models/Pagination";
 
 export interface PaginationFunctions {
-    setDataCount: (dataCount: number) => void;
-    setPage: (page: number) => void;
-    nextPage: () => void;
-    prevPage: () => void;
-    setDataPerPage: (dataPerPage: number) => void;
-};
+  setDataCount: (dataCount: number) => void;
+  setPage: (page: number) => void;
+  nextPage: () => void;
+  prevPage: () => void;
+  setDataPerPage: (dataPerPage: number) => void;
+}
 
 export const usePagination = () => {
-    const [paginationData, setPaginationData] = useState<PaginationData>({
-        dataCount: 0,
-        currentPage: 1,
-        dataPerPage: 5,
-    });
+  const [paginationData, setPaginationData] = useState<PaginationData>({
+    dataCount: 0,
+    currentPage: 1,
+    dataPerPage: 5,
+  });
 
-    const setDataCount = (dataCount: number) => {
-        setPaginationData((prev) => ({...prev, dataCount: dataCount}));
-    }
+  const setDataCount = (dataCount: number) => {
+    const newMaxPages = Math.ceil(dataCount / paginationData.dataPerPage);
 
-    const setPage = (page: number) => {
-        setPaginationData((prev) => ({...prev, currentPage: page}));
-    }
+    setPaginationData((prev) => ({
+      ...prev,
+      dataCount: dataCount,
+      currentPage:
+        newMaxPages < prev.currentPage ? newMaxPages : prev.currentPage,
+    }));
+  };
 
-    const nextPage = () => {
-        setPaginationData((prev) => ({...prev, currentPage: prev.currentPage+1}));
-    }
+  const setPage = (page: number) => {
+    setPaginationData((prev) => ({ ...prev, currentPage: page }));
+  };
 
-    const prevPage = () => {
-        setPaginationData((prev) => ({...prev, currentPage: prev.currentPage-1}));
-    }
+  const nextPage = () => {
+    setPaginationData((prev) => ({
+      ...prev,
+      currentPage: prev.currentPage + 1,
+    }));
+  };
 
-    const setDataPerPage = (dataPerPage: DataAmountPerPage) => {
-        setPaginationData((prev) => ({...prev, dataPerPage: dataPerPage}));
-    }
+  const prevPage = () => {
+    setPaginationData((prev) => ({
+      ...prev,
+      currentPage: prev.currentPage - 1,
+    }));
+  };
 
-    return {
-        paginationData,
-        paginationFunctions: {setDataCount, setPage, nextPage, prevPage, setDataPerPage} as PaginationFunctions}
-}
+  const setDataPerPage = (dataPerPage: DataAmountPerPage) => {
+    const newMaxPages = Math.ceil(paginationData.dataCount / dataPerPage);
+
+    setPaginationData((prev) => ({
+      ...prev,
+      dataPerPage: dataPerPage,
+      currentPage:
+        newMaxPages < prev.currentPage ? newMaxPages : prev.currentPage,
+    }));
+  };
+
+  return {
+    paginationData,
+    paginationFunctions: {
+      setDataCount,
+      setPage,
+      nextPage,
+      prevPage,
+      setDataPerPage,
+    } as PaginationFunctions,
+  };
+};
