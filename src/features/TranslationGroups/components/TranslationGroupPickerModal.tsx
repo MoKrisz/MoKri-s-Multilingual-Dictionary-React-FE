90d9a9df04f3ed87fe199d634357fc1f6ddd1b2b
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-06-25 22:09:31 +0200

    feat: extend TranslationGroupPickerModal to support adding new translation groups

--- src/features/TranslationGroups/components/TranslationGroupPickerModal.tsx
+++ src/features/TranslationGroups/components/TranslationGroupPickerModal.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import Button from "../../../components/Button";
 import Modal from "../../../components/Modal";
+import TranslationGroupForm from "./TranslationGroupForm";
 
 interface TranslationGroupPickerModal {
   isOpen: boolean;
@@ -9,9 +12,48 @@ const TranslationGroupPickerModal: React.FC<TranslationGroupPickerModal> = ({
   isOpen,
   onClose,
 }) => {
+  const [isCreationView, setIsCreationView] = useState(false);
+
+  let modalBody: React.ReactNode;
+
+  if (isCreationView) {
+    modalBody = (
+      <>
+        <Button
+          onClick={() => {
+            setIsCreationView(false);
+          }}
+        >
+          Back
+        </Button>
+        <TranslationGroupForm />
+      </>
+    );
+  } else {
+    modalBody = (
+      <>
+        OData implementation goes here.
+        <div className="justify-items-center">
+          <p className="p-2 italic">
+            If you couldn't find the translation group you're searching for,
+            maybe it doesn't exist yet.
+          </p>
+          <Button
+            onClick={() => {
+              setIsCreationView(true);
+            }}
+            extraStyle="bg-lincolngreendarker"
+          >
+            Add another word!
+          </Button>
+        </div>
+      </>
+    );
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      Something goes here.
+      {modalBody}
     </Modal>
   );
 };
