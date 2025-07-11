c5f882b5e3f108f3edfaceae31ea0096cd18adf2
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-07-11 19:46:11 +0200

    refactor: deleted unused code from ODataContainer, and changed code format in Tabs component

--- src/components/ODataContainer.tsx
+++ src/components/ODataContainer.tsx
@@ -24,57 +24,6 @@ export interface ODataDisplayComponentWithSortingProps<TData, TSortingState>
   setSortingState: (value: React.SetStateAction<TSortingState>) => void;
 }
 
-// interface ODataContainerBaseProps<
-//   TSearchState,
-//   TSearchActions,
-//   TQuerySearchState
-// > {
-//   queryKeyName: string;
-//   useSearchReducer: () => [TSearchState, React.Dispatch<TSearchActions>];
-//   handleQuerySearch?: (searchState: TSearchState) => TQuerySearchState;
-//   SearchComponent: ComponentType<
-//     ODataSearchComponentProps<TSearchState, TSearchActions>
-//   >;
-// }
-
-// interface ODataContainerWithSortingProps<
-//   TData,
-//   TSearchState,
-//   TSearchActions,
-//   TQuerySearchState,
-//   TSortingState
-// > extends ODataContainerBaseProps<
-//     TSearchState,
-//     TSearchActions,
-//     TQuerySearchState
-//   > {
-//   initialSortingState: TSortingState;
-//   fetchData: (
-//     params: ODataFetcherParams<TSearchState, TSortingState>
-//   ) => Promise<OdataResponse<TData>>;
-//   DisplayComponent: ComponentType<
-//     ODataDisplayComponentWithSortingProps<TData, TSortingState>
-//   >;
-// }
-
-// interface ODataContainerWithoutSortingProps<
-//   TData,
-//   TSearchState,
-//   TSearchActions,
-//   TQuerySearchState
-// > extends ODataContainerBaseProps<
-//     TSearchState,
-//     TSearchActions,
-//     TQuerySearchState
-//   > {
-//   fetchData: (
-//     params: ODataFetcherParams<TSearchState, undefined>
-//   ) => Promise<OdataResponse<TData>>;
-//   DisplayComponent: ComponentType<
-//     ODataDisplayComponentWithoutSortingProps<TData>
-//   >;
-// }
-
 interface ODataContainerProps<
   TData,
   TSearchState,
@@ -163,10 +112,8 @@ const ODataContainer = <
     setSortingState(value as React.SetStateAction<TSortingState | undefined>);
   };
 
-  console.log("odata container", data);
-
   return (
-    <div>
+    <div className="w-full">
       <SearchComponent searchState={searchState} dispatch={searchDispatch} />
       {isSortable(DisplayComponent, initialSortingState) ? (
         <DisplayComponent
@@ -188,122 +135,3 @@ const ODataContainer = <
 };
 
 export default ODataContainer;
-
-// export const ODataContainerWithSorting = <
-//   TData,
-//   TSearchState,
-//   TSearchActions,
-//   TQuerySearchState,
-//   TSortingState
-// >({
-//   queryKeyName,
-//   initialSortingState,
-//   fetchData,
-//   useSearchReducer,
-//   handleQuerySearch,
-//   SearchComponent,
-//   DisplayComponent,
-// }: ODataContainerWithSortingProps<
-//   TData,
-//   TSearchState,
-//   TSearchActions,
-//   TQuerySearchState,
-//   TSortingState
-// >) => {
-//   const { paginationData, paginationFunctions } = usePagination();
-//   const [sortingState, setSortingState] = useState(initialSortingState);
-//   const [searchState, searchDispatch] = useSearchReducer();
-
-//   const querySearchState = handleQuerySearch
-//     ? handleQuerySearch(searchState)
-//     : searchState;
-
-//   const querySearchStateJson = JSON.stringify(querySearchState);
-
-//   useEffect(() => {
-//     if (paginationData.currentPage > 1) {
-//       paginationFunctions.setPage(1);
-//     }
-//   }, [querySearchStateJson]);
-
-//   const { data, isPending, isError } = useODataQuery({
-//     queryKeyName,
-//     paginationData,
-//     paginationFunctions,
-//     searchState,
-//     querySearchState,
-//     sortingState,
-//     fetchData,
-//   });
-
-//   return (
-//     <div className="mt-5">
-//       <SearchComponent searchState={searchState} dispatch={searchDispatch} />
-//       <DisplayComponent
-//         data={data}
-//         isPending={isPending}
-//         isError={isError}
-//         sortingState={sortingState}
-//         setSortingState={setSortingState}
-//       />
-//       <Pagination
-//         paginationData={paginationData}
-//         paginationFunctions={paginationFunctions}
-//       />
-//     </div>
-//   );
-// };
-
-// export const ODataContainerWithoutSorting = <
-//   TData,
-//   TSearchState,
-//   TSearchActions,
-//   TQuerySearchState
-// >({
-//   queryKeyName,
-//   fetchData,
-//   useSearchReducer,
-//   handleQuerySearch,
-//   SearchComponent,
-//   DisplayComponent,
-// }: ODataContainerWithoutSortingProps<
-//   TData,
-//   TSearchState,
-//   TSearchActions,
-//   TQuerySearchState
-// >) => {
-//   const { paginationData, paginationFunctions } = usePagination();
-//   const [searchState, searchDispatch] = useSearchReducer();
-
-//   const querySearchState = handleQuerySearch
-//     ? handleQuerySearch(searchState)
-//     : searchState;
-
-//   const querySearchStateJson = JSON.stringify(querySearchState);
-
-//   useEffect(() => {
-//     if (paginationData.currentPage > 1) {
-//       paginationFunctions.setPage(1);
-//     }
-//   }, [querySearchStateJson]);
-
-//   const { data, isPending, isError } = useODataQuery({
-//     queryKeyName,
-//     paginationData,
-//     paginationFunctions,
-//     searchState,
-//     querySearchState,
-//     fetchData,
-//   });
-
-//   return (
-//     <div className="mt-5">
-//       <SearchComponent searchState={searchState} dispatch={searchDispatch} />
-//       <DisplayComponent data={data} isPending={isPending} isError={isError} />
-//       <Pagination
-//         paginationData={paginationData}
-//         paginationFunctions={paginationFunctions}
-//       />
-//     </div>
-//   );
-// };
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -6,7 +6,7 @@ interface TabsProps {
   defaultTab?: string;
 }
 
-export default function Tabs({ tabs, defaultTab }: TabsProps) {
+const Tabs: React.FC<TabsProps> = ({ tabs, defaultTab }) => {
   const [activeTab, setActiveTab] = useState(defaultTab ?? tabs[0].key);
 
   const activeTabFormat = "bg-lincolngreendarker border-b-lincolngreendarker";
@@ -17,7 +17,9 @@ export default function Tabs({ tabs, defaultTab }: TabsProps) {
       <div>
         {tabs.map((tab) => (
           <button
-            className={`-mb-px px-5 rounded-t-xl border border-black ${tab.key === activeTab ? activeTabFormat : nonActiveTabFormat}`}
+            className={`-mb-px px-5 rounded-t-xl border border-black ${
+              tab.key === activeTab ? activeTabFormat : nonActiveTabFormat
+            }`}
             key={tab.key}
             onClick={() => setActiveTab(tab.key)}
           >
@@ -30,4 +32,6 @@ export default function Tabs({ tabs, defaultTab }: TabsProps) {
       </div>
     </>
   );
-}
+};
+
+export default Tabs;
