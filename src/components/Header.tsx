1067d72a32f31b520b6401d96e7dad3d24850330
MoKrisz <79792553+MoKrisz@users.noreply.github.com>
2025-07-29 08:16:51 +0200

    feat: Implement backdrop for the submenu and stop the submenu from pushing the page downwards

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,66 +26,77 @@ export default function Header() {
   };
 
   return (
-    <header className={`flex-col`} onMouseLeave={() => handleMouseLeave()}>
-      <div className="p-2 flex bg-lincolngreendarker shadow-md">
-        <Link
-          to="/"
-          className="text-sm font-bold leading-tight whitespace-nowrap"
-        >
-          MoKri's <br /> Multilingual Dictionary
-        </Link>
-        <nav className="flex items-end justify-evenly w-full">
-          {menuItems.map((item) => (
-            <MenuItem
-              key={item.label}
-              item={item}
-              onHoverStart={() => handleMouseEnter(item.label)}
-            />
-          ))}
-        </nav>
-      </div>
-      <AnimatePresence>
-        {(activeMenuItem || exitingMenuItem) && (
-          <motion.div
-            layout
-            key="submenu"
-            initial={{ height: 0 }}
-            animate={{
-              height: currentSubmenu?.height,
-            }}
-            exit={{ height: 0 }}
-            transition={{ duration: 0.2 }}
-            className="bg-lincolngreendarker overflow-hidden"
+    <>
+      <div
+        key="backdrop"
+        className={`transition-opacity duration-200 ${
+          activeMenuItem && hasSubmenuItems
+            ? "opacity-100 fixed left-0 right-0 bottom-0 top-0 backdrop-blur-sm"
+            : "opacity-0"
+        }`}
+      />
+      <header className={`flex-col relative`} onMouseLeave={handleMouseLeave}>
+        <div className="p-2 flex bg-lincolngreendarker shadow-md">
+          <Link
+            to="/"
+            className="text-sm font-bold leading-tight whitespace-nowrap"
           >
-            <AnimatePresence
-              mode="wait"
-              onExitComplete={() => setExitingMenuItem(null)}
+            MoKri's <br /> Multilingual Dictionary
+          </Link>
+          <nav className="flex items-end justify-evenly w-full">
+            {menuItems.map((item) => (
+              <MenuItem
+                key={item.label}
+                item={item}
+                onHoverStart={() => handleMouseEnter(item.label)}
+              />
+            ))}
+          </nav>
+        </div>
+
+        <AnimatePresence>
+          {(activeMenuItem || exitingMenuItem) && (
+            <motion.div
+              layout
+              key="submenu"
+              initial={{ height: 0 }}
+              animate={{
+                height: currentSubmenu?.height,
+              }}
+              exit={{ height: 0 }}
+              transition={{ duration: 0.2 }}
+              className="absolute left-0 right-0 bg-lincolngreendarker overflow-hidden shadow-lg"
             >
-              {activeMenuItem && hasSubmenuItems && (
-                <motion.div
-                  key={activeMenuItem}
-                  initial={{ opacity: 0, x: -20 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  exit={{ opacity: 0, x: 20 }}
-                  transition={{ duration: 0.2 }}
-                  className="mx-auto max-w-screen-sm grid grid-cols-4 gap-4 pt-3"
-                >
-                  {currentSubmenu.subItems?.map((sub) => (
-                    <div className="flex justify-center" key={sub.label}>
-                      <Link
-                        to={sub.path}
-                        className="text-sm text-gray-800 hover:text-black hover:font-semibold px-4 transition-colors duration-200"
-                      >
-                        {sub.label}
-                      </Link>
-                    </div>
-                  ))}
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </header>
+              <AnimatePresence
+                mode="wait"
+                onExitComplete={() => setExitingMenuItem(null)}
+              >
+                {activeMenuItem && hasSubmenuItems && (
+                  <motion.div
+                    key={activeMenuItem}
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: 20 }}
+                    transition={{ duration: 0.2 }}
+                    className="mx-auto max-w-screen-sm grid grid-cols-4 gap-4 pt-3"
+                  >
+                    {currentSubmenu.subItems?.map((sub) => (
+                      <div className="flex justify-center" key={sub.label}>
+                        <Link
+                          to={sub.path}
+                          className="text-sm text-gray-800 hover:text-black hover:font-semibold px-4 transition-colors duration-200"
+                        >
+                          {sub.label}
+                        </Link>
+                      </div>
+                    ))}
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </header>
+    </>
   );
 }
