6814c801c9d5ce1cdc5c44dac82526002f4482bf
Molnár Krisztofer <molnar.krisztofer@gmail.com>
2026-05-17 15:03:37 +0200

    feat: implement most of the logic for the guess article reducer

--- src/features/Practices/GuessTheArticle/models.ts
+++ src/features/Practices/GuessTheArticle/models.ts
@@ -2,7 +2,3 @@ export interface GuessArticleWord {
   wordId: number;
   text: string;
 }
-
-export interface GuessArticlePracticeData {
-  practiceWords: GuessArticleWord[];
-}
--- src/features/Practices/GuessTheArticle/state/guessArticleReducer.ts
+++ src/features/Practices/GuessTheArticle/state/guessArticleReducer.ts
@@ -1,53 +1,77 @@
 import { useReducer } from "react";
-import { GuessArticlePracticeData } from "../models";
+import { GuessArticleWord } from "../models";
 
 type GuessArticleState =
-    | { step: "CONFIGURING" }
-    | { step: "LOADING_DATA", wordCount: number; }
-    | { step: "PRACTICE"; wordCount: number; currentIdx: number; answers: string[] }
-    | { step: "RESULTS"; wordCount: number; answers: string[] }
-
-type GuessArticleAction = 
-    | { type: "WORD_COUNT_SELECTED"; count: number }
-    | { type: "LOAD_DATA_SUCCESS"; items: GuessArticlePracticeData}
-    | { type: "LOAD_DATA_ERROR"; message: string }
-    | { type: "WORD_INDEX_REQUESTED"; index: number }
-    | { type: "SET_ANSWER"; value: string }
-    | { type: "SUBMIT"; }
-    | { type: "SUBMIT_SUCCESS"; results: string }
-    | { type: "SUBMIT_ERROR"; message: string }
-
-const guessArticleReducer = (state: GuessArticleState, action: GuessArticleAction): GuessArticleState => 
-{
-    switch(action.type)
-    {
-        case "WORD_COUNT_SELECTED":
-            return { step: "LOADING_DATA", wordCount: action.count }
-        case "LOAD_DATA_SUCCESS":
-            if (state.step === "LOADING_DATA")
-            {
-                return { step: "PRACTICE", wordCount: state.wordCount, currentIdx: 0, answers: [] }
-            }
-            
-            return state;
-        case "LOAD_DATA_ERROR":
-            //TODO:
-            return state
-        case "WORD_INDEX_REQUESTED":
-            if (state.step === "PRACTICE")
-            {
-                return { ...state, currentIdx: action.index }
-            }
-
-            return state;
-        case "SET_ANSWER":
-            //TODO:
-            return state;
-        default:
-            return state;
+  | { step: "CONFIGURING" }
+  | { step: "LOADING_DATA"; wordCount: number }
+  | {
+      step: "PRACTICE";
+      wordCount: number;
+      currentIdx: number;
+      words: GuessArticleWord[];
+      answers: Record<number, string | undefined>;
     }
-}
+  | { step: "RESULTS"; wordCount: number; answers: string[] };
+
+type GuessArticleAction =
+  | { type: "WORD_COUNT_SELECTED"; count: number }
+  | { type: "LOAD_DATA_SUCCESS"; words: GuessArticleWord[] }
+  | { type: "LOAD_DATA_ERROR"; message: string }
+  | { type: "WORD_INDEX_REQUESTED"; index: number }
+  | { type: "SET_ANSWER"; answer: string }
+  | { type: "SUBMIT" }
+  | { type: "SUBMIT_SUCCESS"; results: string }
+  | { type: "SUBMIT_ERROR"; message: string };
+
+const guessArticleReducer = (
+  state: GuessArticleState,
+  action: GuessArticleAction,
+): GuessArticleState => {
+  switch (action.type) {
+    case "WORD_COUNT_SELECTED":
+      if (action.count <= 0) return state;
+
+      return { step: "LOADING_DATA", wordCount: action.count };
+    case "LOAD_DATA_SUCCESS":
+      if (state.step !== "LOADING_DATA") return state;
+
+      return {
+        step: "PRACTICE",
+        wordCount: state.wordCount,
+        currentIdx: 0,
+        words: action.words,
+        answers: {},
+      };
+    case "LOAD_DATA_ERROR":
+      //TODO:
+      console.log(
+        "An error happened at LOAD_DATA_ERROR in guessArticleReducer",
+      );
+      return state;
+    case "WORD_INDEX_REQUESTED":
+      if (state.step !== "PRACTICE") return state;
+
+      if (action.index < 0 || action.index >= state.words.length) return state;
+
+      return { ...state, currentIdx: action.index };
+    case "SET_ANSWER":
+      if (state.step !== "PRACTICE") return state;
+
+      const currentWord = state.words[state.currentIdx];
+
+      return {
+        ...state,
+        //TODO: Should it also go to the next element after an answer?
+        answers: {
+            ...state.answers,
+            [currentWord.wordId]: action.answer
+        }
+       }
+    default:
+      return state;
+  }
+};
 
 export const useGuessArticleReducer = () => {
-    return useReducer(guessArticleReducer, {step: "CONFIGURING"});
-}
+  return useReducer(guessArticleReducer, { step: "CONFIGURING" });
+};
